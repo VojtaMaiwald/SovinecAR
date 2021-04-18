de2938a356f2a11b827e775def26e331d5344226
VojtaMaiwald <72343092+VojtaMaiwald@users.noreply.github.com>
2021-04-18 03:34:28 +0200

    Refactor

--- app/src/main/java/com/vsb/sovinecar/ModelName.java
+++ app/src/main/java/com/vsb/sovinecar/ModelName.java
@@ -5,7 +5,6 @@ public final class ModelName {
     public static final String MERCEDES = "Mercedes";
     public static final String IRONMAN = "Ironman";
     public static final String MUSHROOM = "Mushroom";
-    public static final String MATRIX = "Matrix";
 
     public ModelName() { }
 }
--- app/src/main/java/com/vsb/sovinecar/ModelUri.java
+++ app/src/main/java/com/vsb/sovinecar/ModelUri.java
@@ -6,7 +6,7 @@ public final class ModelUri {
     public static final String MERCEDES = "Mercedes.glb";
     public static final String IRONMAN = "Ironman.glb";
     public static final String MUSHROOM = "Mushroom.glb";
-    public static final String MATRIX = "video_screen.glb";
+    public static final String MATRIX = "Video.glb";
     public static final String RABBIT = "Rabbit.glb";
     public static final String VAULT = "Vault.glb";
 
